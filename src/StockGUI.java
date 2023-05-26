f9f6542351acd9ef42005e10992eda6bcb44128d
school <sample@bths.edu>
2023-05-26 12:30:23 -0400

    im gonna lose it

---
+++ src/StockGUI.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class StockGUI extends JFrame{
+    private final String BASE_URL = "https://api.stockdata.org/v1/data/quote HTTP/1.1";
+    private JButton submitButton;
+    private JTextField Ticket;
+
+    String API = "MafSn36nzGWVW348Ool0wf4s0KlrLe1WfrJz55XO";
+
+    private void createUIComponents(){
+
+    }
+
+}
