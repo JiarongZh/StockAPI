dac906a9712e5c56d3de6cf60e67326433a5e51b
school <student@bths.edu>
2023-06-07 12:29:42 -0400

    I HAVE ISSUES

--- src/StockGUI.java
+++ src/StockGUI.java
@@ -1,6 +1,10 @@
+import org.json.JSONException;
+
 import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.util.HashMap;
+import java.util.Random;
 
 
 public class StockGUI extends JFrame implements ActionListener{
@@ -15,6 +19,7 @@ public class StockGUI extends JFrame implements ActionListener{
     private JTextArea changeText;
     private JTextArea error;
     private JPanel mainPanel;
+    private JButton gamble;
 
     private Stock stock;
 
@@ -30,16 +35,22 @@ public class StockGUI extends JFrame implements ActionListener{
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         submitButton.addActionListener(this);
         Clear.addActionListener(this);
+        gamble.addActionListener(this);
         setVisible(true);
     }
 
     private void loadStock(String name){
-        stock = StockAPI.getStockInfo(name);
-        nameText.setText(stock.getTicker());
-        priceText.setText("$" + stock.getPrice());
-        highText.setText("$" + stock.getHigh());
-        lowText.setText("$" + stock.getLow());
-        changeText.setText(stock.getChange() + "%");
+        try {
+            stock = StockAPI.getStockInfo(name);
+            nameText.setText(stock.getTicker());
+            priceText.setText("$" + stock.getPrice());
+            highText.setText("$" + stock.getHigh());
+            lowText.setText("$" + stock.getLow());
+            changeText.setText(stock.getChange() + "%");
+        }catch (JSONException e){
+            setClear();
+            error.setText("Stock not found or does not exist in this database");
+        }
     }
 
     private void setClear(){
@@ -48,6 +59,25 @@ public class StockGUI extends JFrame implements ActionListener{
         highText.setText("");
         lowText.setText("");
         changeText.setText("");
+        error.setText("");
+    }
+
+    private void gamble(){
+        setClear();
+        boolean bool = true;
+        while(bool){
+            String gambol = "";
+            for(int i = 0; i<4; i++) {
+                Random r = new Random();
+                char c = (char) (r.nextInt(26) + 'a');
+                gambol += c;
+            }
+            System.out.println(gambol);
+            loadStock(gambol.toUpperCase());
+            if(error.getText().length()==0){
+                bool = false;
+            }
+        }
     }
 
 @Override
@@ -57,6 +87,8 @@ public class StockGUI extends JFrame implements ActionListener{
             JButton b = (JButton) a;
             if(b.getText().equals("Submit")){
                 loadStock(Ticket.getText().toUpperCase());
+            }else if(b.getText().equals("Gamble")){
+                gamble();
             }else{
                 setClear();
             }
